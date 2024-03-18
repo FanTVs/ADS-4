88886b00d11b9e5d7f8b8991cf7f3f19af04bc1a
FanTVs <160307521+FanTVs@users.noreply.github.com>
2024-03-18 21:52:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,66 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+    int cout = 0;
+    while (cout < 1000000) {
+        cout++;
+    }
+    int count = 0;
+    for (int i = 0; i < len; i++) {
+        for (int j = i + 1; j < len; ++j) {
+            if ((arr[i] + arr[j]) == value)
+                count++;
+        }
+    }
+    return count;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+    int weight = 0;
+    for (int i = 0; i < len; i++) {
+        weight += i;
+    }
+    while (arr[len] > value) {
+        len--;
+    }
+    int count = 0;
+    for (int i = 0; i < len - 1; i++) {
+        for (int j = len - 1; j > i; j--) {
+            if (arr[i] + arr[j] == value) {
+                count++;
+            } else if (arr[i] + arr[j] < value)
+                break;
+        }
+    }
+    return count;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+    int left;
+    int right, sered, SrZn;
+    for (int i = 0; i < len; ++i) {
+        right = len - 1;
+        left = i + 1;
+        SrZn = value - arr[i];
+        while (left <= right) {
+            sered = (left + right) / 2;
+            if (arr[sered] == SrZn) {
+                count++;
+                int p = sered - 1;
+                while ((p > i) && (arr[p] == arr[sered])) {
+                    count++;
+                    p--;
+                }
+                int p2 = sered + 1;
+                while ((i < p2) && (arr[p2] == arr[sered])) {
+                    count++;
+                    p2++;
+                }
+                break;
+            } else if (arr[sered] > SrZn) {
+                right = SrZn - 1;
+            } else {
+            left = SrZn + 1;
+            }
+        }
+    }
+    return count;
 }
